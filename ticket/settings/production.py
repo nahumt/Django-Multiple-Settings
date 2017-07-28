a0b4e1fc07688612570d065f4a4807312fc1314f
Nahum Trindad <nahum.trinidadv@gmail.com>
2017-07-28 18:25:32 -0500

    feat: Configuration multiple setting

---
+++ ticket/settings/production.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [ ]
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+EMAIL_USE_TLS = True
